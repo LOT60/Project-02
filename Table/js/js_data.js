140ce22a424b74e0f3708dc4701862d5651beaea
ANABGR <anabgr1@gmail.com>
2021-05-12 21:26:31 -0500

    update

---
+++ Table/js/js_data.js
@@ -0,0 +1,601 @@
+var data = [{
+    Date 3/27/2018
+    Open "304"
+    High "304.27"
+    Low "277.18"
+    Close "279.18"
+    Volume "13696168"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "304"
+    Adj. High "304.27"
+    Adj. Low "277.18"
+    Adj. Close "279.18"
+    Adj. Volume "13696168"
+  },
+  {
+    Date 3/26/2018
+    Open "307.34"
+    High "307.59"
+    Low "291.36"
+    Close "304.18"
+    Volume "8324639"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "307.34"
+    Adj. High "307.59"
+    Adj. Low "291.36"
+    Adj. Close "304.18"
+    Adj. Volume "8324639"
+},
+{
+    Date 3/23/2018
+    Open "311.25"
+    High "311.61"
+    Low "300.45"
+    Close "301.54"
+    Volume "6600538"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "311.25"
+    Adj. High "311.61"
+    Adj. Low "300.45"
+    Adj. Close "301.54"
+    Adj. Volume "6600538"
+},
+{
+    Date 3/22/2018
+    Open "313.89"
+    High "318.82"
+    Low "308.18"
+    Close "309.1"
+    Volume "4914307"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "313.89"
+    Adj. High "318.82"
+    Adj. Low "308.18"
+    Adj. Close "309.1"
+    Adj. Volume "4914307"
+},
+{
+    Date 3/21/2018
+    Open "310.25"
+    High "322.44"
+    Low "310.19"
+    Close "316.53"
+    Volume "5927881"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "310.25"
+    Adj. High "322.44"
+    Adj. Low "310.19"
+    Adj. Close "316.53"
+    Adj. Volume "5927881"
+},
+{
+    Date 3/20/2018
+    Open "314.87"
+    High "316.25"
+    Low "308.76"
+    Close "310.55"
+    Volume "4726182"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "314.87"
+    Adj. High "316.25"
+    Adj. Low "308.76"
+    Adj. Close "310.55"
+    Adj. Volume "4726182"
+},
+{
+    Date 3/19/2018
+    Open "316.5"
+    High "320.75"
+    Low "309.67"
+    Close "313.56"
+    Volume "7440766"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "316.5"
+    Adj. High "320.75"
+    Adj. Low "309.67"
+    Adj. Close "313.56"
+    Adj. Volume "7440766"
+},
+{
+    Date 3/16/2018
+    Open "325.96"
+    High "327.4"
+    Low "319.07"
+    Close "321.35"
+    Volume "6058612"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "325.96"
+    Adj. High "327.4"
+    Adj. Low "319.07"
+    Adj. Close "321.35"
+    Adj. Volume "6058612"
+},
+{
+    Date 3/15/2018
+    Open "329.38"
+    High "332.85"
+    Low "321.1"
+    Close "325.6"
+    Volume "6366922"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "329.38"
+    Adj. High "332.85"
+    Adj. Low "321.1"
+    Adj. Close "325.6"
+    Adj. Volume "6366922"
+},
+{
+    Date 3/14/2018
+    Open "336.76"
+    High "339.81"
+    Low "323.93"
+    Close "326.63"
+    Volume "7932511"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "336.76"
+    Adj. High "339.81"
+    Adj. Low "323.93"
+    Adj. Close "326.63"
+    Adj. Volume "7932511"
+},
+{
+    Date 3/13/2018
+    Open "344.92"
+    High "345.12"
+    Low "336.2635"
+    Close "341.84"
+    Volume "5932110"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "344.92"
+    Adj. High "345.12"
+    Adj. Low "336.2635"
+    Adj. Close "341.84"
+    Adj. Volume "5932110"
+},
+{
+    Date 3/12/2018
+    Open "328.61"
+    High "347.21"
+    Low "326.5"
+    Close "345.51"
+    Volume "8212156"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "328.61"
+    Adj. High "347.21"
+    Adj. Low "326.5"
+    Adj. Close "345.51"
+    Adj. Volume "8212156"
+},
+{
+    Date 3/9/2018
+    Open "324.1"
+    High "328.49"
+    Low "322.37"
+    Close "327.17"
+    Volume "5479716"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "324.1"
+    Adj. High "328.49"
+    Adj. Low "322.37"
+    Adj. Close "327.17"
+    Adj. Volume "5479716"
+},
+{
+    Date 3/8/2018
+    Open "332.86"
+    High "333.3"
+    Low "326.274"
+    Close "329.1"
+    Volume "3476944"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "332.86"
+    Adj. High "333.3"
+    Adj. Low "326.274"
+    Adj. Close "329.1"
+    Adj. Volume "3476944"
+},
+{
+    Date 3/7/2018
+    Open "325.44"
+    High "332.5"
+    Low "321.74"
+    Close "332.3"
+    Volume "5007297"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "325.44"
+    Adj. High "332.5"
+    Adj. Low "321.74"
+    Adj. Close "332.3"
+    Adj. Volume "5007297"
+},
+{
+    Date 3/6/2018
+    Open "333.75"
+    High "336.37"
+    Low "327.03"
+    Close "328.2"
+    Volume "4285744"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "333.75"
+    Adj. High "336.37"
+    Adj. Low "327.03"
+    Adj. Close "328.2"
+    Adj. Volume "4285744"
+},
+{
+    Date 3/5/2018
+    Open "332.39"
+    High "337.75"
+    Low "329.2929"
+    Close "333.35"
+    Volume "3823769"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "332.39"
+    Adj. High "337.75"
+    Adj. Low "329.2929"
+    Adj. Close "333.35"
+    Adj. Volume "3823769"
+},
+{
+    Date 3/2/2018
+    Open "326.98"
+    High "335.22"
+    Low "322.97"
+    Close "335.12"
+    Volume "5092829"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "326.98"
+    Adj. High "335.22"
+    Adj. Low "322.97"
+    Adj. Close "335.12"
+    Adj. Volume "5092829"
+},
+{
+    Date 3/1/2018
+    Open "345.01"
+    High "348.67"
+    Low "330.07"
+    Close "330.93"
+    Volume "6885601"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "345.01"
+    Adj. High "348.67"
+    Adj. Low "330.07"
+    Adj. Close "330.93"
+    Adj. Volume "6885601"
+},
+{
+    Date 2/28/2018
+    Open "352.57"
+    High "355.24"
+    Low "342.22"
+    Close "343.06"
+    Volume "6042895"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "352.57"
+    Adj. High "355.24"
+    Adj. Low "342.22"
+    Adj. Close "343.06"
+    Adj. Volume "6042895"
+},
+{
+    Date 3/27/2017
+    Open "304"
+    High "304.27"
+    Low "277.18"
+    Close "279.18"
+    Volume "13696168"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "304"
+    Adj. High "304.27"
+    Adj. Low "277.18"
+    Adj. Close "279.18"
+    Adj. Volume "13696168"
+},
+{
+    Date 3/26/2017
+    Open "307.34"
+    High "307.59"
+    Low "291.36"
+    Close "304.18"
+    Volume "8324639"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "307.34"
+    Adj. High "307.59"
+    Adj. Low "291.36"
+    Adj. Close "304.18"
+    Adj. Volume "8324639"
+},
+{
+    Date 3/23/2017
+    Open "311.25"
+    High "311.61"
+    Low "300.45"
+    Close "301.54"
+    Volume "6600538"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "311.25"
+    Adj. High "311.61"
+    Adj. Low "300.45"
+    Adj. Close "301.54"
+    Adj. Volume "6600538"
+},
+{
+    Date 3/22/2017
+    Open "313.89"
+    High "318.82"
+    Low "308.18"
+    Close "309.1"
+    Volume "4914307"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "313.89"
+    Adj. High "318.82"
+    Adj. Low "308.18"
+    Adj. Close "309.1"
+    Adj. Volume "4914307"
+},
+{
+    Date 3/21/2017
+    Open "310.25"
+    High "322.44"
+    Low "310.19"
+    Close "316.53"
+    Volume "5927881"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "310.25"
+    Adj. High "322.44"
+    Adj. Low "310.19"
+    Adj. Close "316.53"
+    Adj. Volume "5927881"
+},
+{
+    Date 3/20/2017
+    Open "314.87"
+    High "316.25"
+    Low "308.76"
+    Close "310.55"
+    Volume "4726182"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "314.87"
+    Adj. High "316.25"
+    Adj. Low "308.76"
+    Adj. Close "310.55"
+    Adj. Volume "4726182"
+},
+{
+    Date 3/19/2017
+    Open "316.5"
+    High "320.75"
+    Low "309.67"
+    Close "313.56"
+    Volume "7440766"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "316.5"
+    Adj. High "320.75"
+    Adj. Low "309.67"
+    Adj. Close "313.56"
+    Adj. Volume "7440766"
+},
+{
+    Date 3/16/2017
+    Open "325.96"
+    High "327.4"
+    Low "319.07"
+    Close "321.35"
+    Volume "6058612"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "325.96"
+    Adj. High "327.4"
+    Adj. Low "319.07"
+    Adj. Close "321.35"
+    Adj. Volume "6058612"
+},
+{
+    Date 3/15/2017
+    Open "329.38"
+    High "332.85"
+    Low "321.1"
+    Close "325.6"
+    Volume "6366922"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "329.38"
+    Adj. High "332.85"
+    Adj. Low "321.1"
+    Adj. Close "325.6"
+    Adj. Volume "6366922"
+},
+{
+    Date 3/14/2017
+    Open "336.76"
+    High "339.81"
+    Low "323.93"
+    Close "326.63"
+    Volume "7932511"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "336.76"
+    Adj. High "339.81"
+    Adj. Low "323.93"
+    Adj. Close "326.63"
+    Adj. Volume "7932511"
+},
+{
+    Date 3/13/2017
+    Open "344.92"
+    High "345.12"
+    Low "336.2635"
+    Close "341.84"
+    Volume "5932110"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "344.92"
+    Adj. High "345.12"
+    Adj. Low "336.2635"
+    Adj. Close "341.84"
+    Adj. Volume "5932110"
+},
+{
+    Date 3/12/2017
+    Open "328.61"
+    High "347.21"
+    Low "326.5"
+    Close "345.51"
+    Volume "8212156"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "328.61"
+    Adj. High "347.21"
+    Adj. Low "326.5"
+    Adj. Close "345.51"
+    Adj. Volume "8212156"
+},
+{
+    Date 3/9/2017
+    Open "324.1"
+    High "328.49"
+    Low "322.37"
+    Close "327.17"
+    Volume "5479716"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "324.1"
+    Adj. High "328.49"
+    Adj. Low "322.37"
+    Adj. Close "327.17"
+    Adj. Volume "5479716"
+},
+{
+    Date 3/8/2017
+    Open "332.86"
+    High "333.3"
+    Low "326.274"
+    Close "329.1"
+    Volume "3476944"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "332.86"
+    Adj. High "333.3"
+    Adj. Low "326.274"
+    Adj. Close "329.1"
+    Adj. Volume "3476944"
+},
+{
+    Date 3/7/2017
+    Open "325.44"
+    High "332.5"
+    Low "321.74"
+    Close "332.3"
+    Volume "5007297"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "325.44"
+    Adj. High "332.5"
+    Adj. Low "321.74"
+    Adj. Close "332.3"
+    Adj. Volume "5007297"
+},
+{
+    Date 3/6/2017
+    Open "333.75"
+    High "336.37"
+    Low "327.03"
+    Close "328.2"
+    Volume "4285744"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "333.75"
+    Adj. High "336.37"
+    Adj. Low "327.03"
+    Adj. Close "328.2"
+    Adj. Volume "4285744"
+},
+{
+    Date 3/5/2017
+    Open "332.39"
+    High "337.75"
+    Low "329.2929"
+    Close "333.35"
+    Volume "3823769"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "332.39"
+    Adj. High "337.75"
+    Adj. Low "329.2929"
+    Adj. Close "333.35"
+    Adj. Volume "3823769"
+},
+{
+    Date 3/2/2017
+    Open "326.98"
+    High "335.22"
+    Low "322.97"
+    Close "335.12"
+    Volume "5092829"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "326.98"
+    Adj. High "335.22"
+    Adj. Low "322.97"
+    Adj. Close "335.12"
+    Adj. Volume "5092829"
+},
+{
+    Date 3/1/2017
+    Open "345.01"
+    High "348.67"
+    Low "330.07"
+    Close "330.93"
+    Volume "6885601"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "345.01"
+    Adj. High "348.67"
+    Adj. Low "330.07"
+    Adj. Close "330.93"
+    Adj. Volume "6885601"
+},
+{
+    Date 2/28/2017
+    Open "352.57"
+    High "355.24"
+    Low "342.22"
+    Close "343.06"
+    Volume "6042895"
+    Ex-Dividend "0"
+    Split Ratio "1"
+    Adj. Open "352.57"
+    Adj. High "355.24"
+    Adj. Low "342.22"
+    Adj. Close "343.06"
+    Adj. Volume "6042895" 
+  }
+];
